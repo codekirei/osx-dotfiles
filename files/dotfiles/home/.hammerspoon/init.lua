1e45af88928fe86e0b789da8493dd8bdc964ad72
Jacob Blakely <jacob@codekirei.com>
2016-06-19 20:43:52 -0700

    improve window movement in hammerspoon

--- files/dotfiles/home/.hammerspoon/init.lua
+++ files/dotfiles/home/.hammerspoon/init.lua
@@ -1,27 +1,44 @@
+------------------------------------------------------------
+-- globals
+------------------------------------------------------------
+scale = 50
+
+------------------------------------------------------------
+-- meta
+------------------------------------------------------------
 hs.hotkey.bind(
-  {"cmd", "alt", "ctrl"}, "W",
+  {'cmd', 'alt', 'ctrl'}, 'R',
   function()
+    hs.reload()
     hs.notify.new({
-      title="Hammerspoon",
-      informativeText="It's alive!",
+      title='Hammerspoon',
+      informativeText='Config reloaded!',
     }):send()
   end
 )
 
-hs.hotkey.bind(
-  {"cmd", "alt", "ctrl"}, "R",
-  function()
-    hs.reload()
+------------------------------------------------------------
+-- windows
+------------------------------------------------------------
+function moveWindow(delta)
+  local win = hs.window.focusedWindow()
+  local frame = win:frame()
+  for k,v in pairs(delta) do
+    frame[k] = frame[k] + v * scale
   end
-)
+  win:setFrame(frame)
+end
 
--- move window left
-hs.hotkey.bind(
-  {"cmd", "ctrl"}, "H",
-  function()
-    local win = hs.window.focusedWindow()
-    local frame = win:frame()
-    frame.x = frame.x - 10
-    win:setFrame(frame)
-  end
-)
+for k,v in pairs({
+  H = { x = -1 },
+  J = { y = 1 },
+  K = { y = -1 },
+  L = { x = 1 },
+}) do
+  hs.hotkey.bind(
+    {'cmd', 'ctrl'}, k,
+    function()
+      moveWindow(v)
+    end
+  )
+end
